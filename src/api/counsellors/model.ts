6d9cfc15ff698472ae988915dec2c20ff4e43c8d
mkissarli <kissarlim@gmail.com>
2020-09-22 18:27:08 +0100

    Added missing data tests, and made the model conform to it.

--- src/api/counsellors/model.ts
+++ src/api/counsellors/model.ts
@@ -8,30 +8,39 @@ export const counsellor_schema = new db.Schema({
   },
   first_name: {
     type: String,
-    required: true
+    required: true,
+    default: undefined
   },
   last_name: {
     type: String,
-    required: true
+    required: true,
+    default: undefined
   },
-  appointment_types: [{
-    type: String,
-    enum: ["consultation", "one_off"],
-    required: true
-  }],
-  appointment_mediums: [{
-    type: String,
-    enum: ["phone", "video"],
-    required: true
-  }],
+  appointment_types: {
+    type: [{
+      type: String,
+      enum: ["consultation", "one_off"],
+      default: undefined
+    }],
+    validate: (v: any) => Array.isArray(v) && v.length > 0,
+  },
+  appointment_mediums: {
+    type: [{
+      type: String,
+      enum: ["phone", "video"],
+      default: undefined
+    }],
+    validate: (v: any) => Array.isArray(v) && v.length > 0,
+  },
   availability: {
     type: [appointment_schema]
   }
 });
 
-//counsellor_schema.pre("save", function() {
-//  this.markModified("avaliability");
-//});
+counsellor_schema.pre("save", function() {
+  this.markModified("avaliability");
+
+});
 
 export interface ICounsellor extends db.Document {
   counsellor_id: {
@@ -39,22 +48,30 @@ export interface ICounsellor extends db.Document {
   },
   first_name: {
     type: String,
-    required: true
+    required: true,
+    default: undefined
   },
   last_name: {
     type: String,
+    required: true,
+    default: undefined
+  },
+  appointment_types: {
+    type: [{
+      type: String,
+      enum: ["consultation", "one_off"],
+      default: undefined
+    }],
     required: true
   },
-  appointment_types: [{
-    type: String,
-    enum: ["consultation", "one_off"],
+  appointment_mediums: {
+    type: [{
+      type: String,
+      enum: ["phone", "video"],
+      default: undefined
+    }],
     required: true
-  }],
-  appointment_mediums: [{
-    type: String,
-    enum: ["phone", "video"],
-    required: true
-  }],
+  },
   availability: [IAppointment]  
 }
 
--- tests/api/counsellors/model.test.ts
+++ tests/api/counsellors/model.test.ts
@@ -2,7 +2,7 @@ import { counsellor_model } from '../../../src/api/counsellors/model';
 import db from 'mongoose';
 import { appointment_model } from '../../../src/api/appointments/model';
 
-const valid_counsellor_data = {
+const valid_data = {
   "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
   "first_name": "Lettie",
   "last_name": "Wolland",
@@ -19,7 +19,7 @@ const valid_counsellor_data = {
     }]
   };
 
-const no_avaliability_counsellor_data = {
+const no_avaliability_data = {
   "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
   "first_name": "Lettie",
   "last_name": "Wolland",
@@ -27,9 +27,25 @@ const no_avaliability_counsellor_data = {
   "appointment_mediums": ["phone"]
   };
 
-const outdated_counsellor_data = {
+const outdated_data = {
   "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
   "first_name": "Lettie",
+  "last_name": "Wolland",
+  "appointment_types": ["consultation", "one_off"],
+  "appointment_mediums": ["phone"],
+  "availability": [
+    {
+      "id": "88ZAQZbhu2Hf7CyVUmASLM",
+      "datetime": "2020-10-25T19:00:00.000Z"
+    },
+    {
+      "id": "jLYfhekg7wjZk3JyRH35eX",
+      "datetime": "1980-10-15T21:00:00.000Z"
+    }]
+  };
+
+const missing_f_name_data = {
+  "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
   "last_name": "Wolland",
   "appointment_types": ["consultation", "one_off"],
   "appointment_mediums": ["phone"],
@@ -44,24 +60,7 @@ const outdated_counsellor_data = {
     }]
   };
 
-const missing_f_name_counsellor_data = {
-  "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
-  "first_name": "Lettie",
-  "last_name": "Wolland",
-  "appointment_types": ["consultation", "one_off"],
-  "appointment_mediums": ["phone"],
-  "availability": [
-    {
-      "id": "88ZAQZbhu2Hf7CyVUmASLM",
-      "datetime": "2020-10-25T19:00:00.000Z"
-    },
-    {
-      "id": "jLYfhekg7wjZk3JyRH35eX",
-      "datetime": "2020-10-15T21:00:00.000Z"
-    }]
-  };
-
-const missing_l_name_counsellor_data = {
+const missing_l_name_data = {
   "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
     "first_name": "Lettie",
     "appointment_types": ["consultation", "one_off"],
@@ -77,7 +76,7 @@ const missing_l_name_counsellor_data = {
       }]
     };
 
-const missing_app_types_counsellor_data = {
+const missing_app_types_data = {
   "counsellor_id": "79590113-a6a3-45c3-9d5e-28472a8c4a74",
   "first_name": "Lettie",
   "last_name": "Wolland",
@@ -154,26 +153,85 @@ describe("Counsellor Model Test", () => {
   });
 
   it("create and save a new counsellor successfully", async () => {
-
-    const valid_counsellor = new counsellor_model(valid_counsellor_data); 
-    valid_counsellor.markModified("avaliability");
+    const valid_counsellor = new counsellor_model(valid_data); 
     const saved_counsellor = await valid_counsellor.save();
 
     expect(saved_counsellor._id).toBeDefined();
     // Not sure if this should be passed in or generated. It's kind of useless in mongodb.
     expect(saved_counsellor.counsellor_id).toBeDefined();
-    expect(saved_counsellor.first_name).toBe(valid_counsellor_data.first_name);
-    expect(saved_counsellor.last_name).toBe(valid_counsellor_data.last_name);
+    expect(saved_counsellor.first_name).toBe(valid_data.first_name);
+    expect(saved_counsellor.last_name).toBe(valid_data.last_name);
 
     // toString() here as dealing with CoreMongooseArray proved to be a headache.
-    expect(saved_counsellor.appointment_types.toString()).toBe(valid_counsellor_data.appointment_types.toString());
-    expect(saved_counsellor.appointment_mediums.toString()).toBe(valid_counsellor_data.appointment_mediums.toString());
+    expect(saved_counsellor.appointment_types.toString()).toBe(valid_data.appointment_types.toString());
+    expect(saved_counsellor.appointment_mediums.toString()).toBe(valid_data.appointment_mediums.toString());
 
     // Others we get _id messing up everything. I should probs remove that.
     for(let i = 0; i < saved_counsellor.availability.length; ++i){
-      expect(saved_counsellor.availability[i].id).toBe(valid_counsellor_data.availability[i].id);
-      expect(saved_counsellor.availability[i].datetime).toStrictEqual(new Date(valid_counsellor_data.availability[i].datetime));
+      expect(saved_counsellor.availability[i].id).toBe(valid_data.availability[i].id);
+      expect(saved_counsellor.availability[i].datetime).toStrictEqual(new Date(valid_data.availability[i].datetime));
     }
   });
 
+  it("creates and saves a new counsellor successfully when there is no avaliability", async () => {
+    const valid_counsellor = new counsellor_model(no_avaliability_data); 
+    const saved_counsellor = await valid_counsellor.save();
+
+    expect(saved_counsellor._id).toBeDefined();
+    // Not sure if this should be passed in or generated. It's kind of useless in mongodb.
+    expect(saved_counsellor.counsellor_id).toBeDefined();
+    expect(saved_counsellor.first_name).toBe(no_avaliability_data.first_name);
+    expect(saved_counsellor.last_name).toBe(no_avaliability_data.last_name);
+
+    // toString() here as dealing with CoreMongooseArray proved to be a headache.
+    expect(saved_counsellor.appointment_types.toString()).toBe(no_avaliability_data.appointment_types.toString());
+    expect(saved_counsellor.appointment_mediums.toString()).toBe(no_avaliability_data.appointment_mediums.toString());
+
+    expect(saved_counsellor.availability).toHaveLength(0);
+  })
+
+  it("to fail if the some of the avaliability is outdated", async () => {
+    const invalid_counsellor = new counsellor_model(outdated_data); 
+    const saved_counsellor = invalid_counsellor.save();
+
+    await expect(saved_counsellor)
+      .rejects
+      .toThrow(db.Error.ValidationError);
+  })
+
+  it("to fail if first_name is missing", async () => {
+    const invalid_counsellor = new counsellor_model(missing_f_name_data); 
+    const saved_counsellor = invalid_counsellor.save();
+
+    await expect(saved_counsellor)
+      .rejects
+      .toThrow(db.Error.ValidationError);
+  })
+
+  it("to fail if last_name is missing", async () => {
+    const invalid_counsellor = new counsellor_model(missing_f_name_data); 
+    const saved_counsellor = invalid_counsellor.save();
+
+    await expect(saved_counsellor)
+      .rejects
+      .toThrow(db.Error.ValidationError);
+  })
+
+  it("to fail if appointment_types is missing", async () => {
+    const invalid_counsellor = new counsellor_model(missing_app_types_data); 
+    const saved_counsellor = invalid_counsellor.save();
+
+    await expect(saved_counsellor)
+      .rejects
+      .toThrow(db.Error.ValidationError);
+  })
+
+  it("to fail if appointment_mediums is missing", async () => {
+    const invalid_counsellor = new counsellor_model(missing_app_mediums_data); 
+    const saved_counsellor = invalid_counsellor.save();
+
+    await expect(saved_counsellor)
+      .rejects
+      .toThrow(db.Error.ValidationError);
+  })
 })
